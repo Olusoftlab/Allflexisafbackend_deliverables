341c18be29f44a04a525dc188aca3dbb4280f38c
olumild <olumild998@gmail.com>
2026-06-16 17:16:49 +0100

    begin Deliverable_7

---
+++ Deliverable_7/primitivetypes/src/primitivetypes/App.java
@@ -0,0 +1,10 @@
+package primitivetypes;
+@SuppressWarnings("java:S106")
+
+
+public class App {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+        System.out.println("practicing primitives");
+    }
+}
