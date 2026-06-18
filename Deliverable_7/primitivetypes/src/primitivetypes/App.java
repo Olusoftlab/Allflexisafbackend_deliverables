36620250c06de54187060422a26aeeac7f7a3fa6
olumild <olumild998@gmail.com>
2026-06-18 06:32:31 +0100

    reverse word

--- Deliverable_7/primitivetypes/src/primitivetypes/App.java
+++ Deliverable_7/primitivetypes/src/primitivetypes/App.java
@@ -1,10 +1,34 @@
 package primitivetypes;
+
 @SuppressWarnings("java:S106")
 
-
 public class App {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("practicing primitives");
+
+        String myWord = "Hello World";
+
+        int len = myWord.length();
+
+        char[] myWordArr = new char[len];
+        char[] reverseArr = new char[len];
+
+        for (int i = 0; i < len; i++) {
+
+            myWordArr[i] = myWord.charAt(i);
+
+        }
+
+        for (int x = 0; x < len; x++) {
+
+            reverseArr[x] = myWordArr[len - 1 - x];
+
+        }
+
+        String reverseWord = new String(reverseArr);
+
+        System.out.println(reverseWord);
+
     }
 }
