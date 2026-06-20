3f6a8b00c4a5c791f36979817182c8513ddc1cac
olumild <olumild998@gmail.com>
2026-06-20 12:09:16 +0100

    beginning packages

---
+++ Deliverable_8/packagename/src/packagename/App.java
@@ -0,0 +1,10 @@
+package packagename;
+@SuppressWarnings("java:S106")
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println("working with packages");
+    }
+}
